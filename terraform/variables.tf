00b2613b31febd18c8b939d01b70eabdb7ceef52
Chomba-Mumba <chombaibex@gmail.com>
2025-02-19 10:39:35 +0000

    chore: updated tf  variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,28 +6,22 @@ variable "aws_region" {
   default = "eu-west-2"
 }
 
-variable "s3_bucket_arn" {
-  description = "ARN of the S3 bucket to store the lambda function code."
-
-  type = string
-}
-
 #API GATEWAY VARIABLES
 
 variable "rest_api_name" {
   description = "The name of your API"
   type        = string
-  default     = "stock-market-prediction-api"
+  default     = "stock_market_prediction_api"
 }
 
 variable "rest_api_description" {
   description = "The name of your API"
   type        = string
-  default     = "stock-market-prediction-api"
+  default     = "stock_market_prediction_api"
 }
 
 variable "stage_name" {
   description = "The name of your API stage"
   type        = string
-  default     = "stock-market-prediction-stage"
+  default     = "stock_market_prediction_stage"
 }
