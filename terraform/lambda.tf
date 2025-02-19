debc2f13675ffb7c73fa4f5badd90973a8175906
Chomba-Mumba <chombaibex@gmail.com>
2025-02-19 17:51:39 +0000

    push

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -9,7 +9,7 @@ resource "aws_lambda_function" "predictions_lambda" {
 
   s3_bucket  = "stock-market-site"
   s3_key     = "lambda/predictions_lambda.zip"
-  depends_on = [aws_iam_role.stock_market_lambda_role, aws_iam_policy.stock_market_iam_policy_for_lambda]
+  depends_on = [aws_iam_role.stock_market_lambda_role]
 }
 resource "aws_cloudwatch_log_group" "predictions" {
   name = "/aws/lambda/${aws_lambda_function.predictions_lambda.function_name}"
