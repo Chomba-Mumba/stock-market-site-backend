36e9033495182c27a3300946fbf7dca03f0fa46d
Chomba-Mumba <chombaibex@gmail.com>
2025-02-19 17:39:42 +0000

    chore

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -2,18 +2,21 @@
 resource "aws_iam_role" "stock_market_lambda_role" {
   name = "stock_market_lambda_role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "lambda.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
 }
 
 resource "aws_iam_policy" "stock_market_iam_policy_for_lambda" {
@@ -21,7 +24,7 @@ resource "aws_iam_policy" "stock_market_iam_policy_for_lambda" {
   name        = "aws_iam_policy_for_terraform_lambda"
   path        = "/"
   description = "AWS IAM Policy for managing aws lambda role"
-  policy      = <<EOF
+  policy      =  policy = <<EOF
 {
  "Version": "2012-10-17",
  "Statement": [
@@ -33,46 +36,7 @@ resource "aws_iam_policy" "stock_market_iam_policy_for_lambda" {
      ],
      "Resource": "arn:aws:logs:*:*:*",
      "Effect": "Allow"
-   },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:ListBucket"
-      ],
-      "Resource": "arn:aws:s3:::stock-market-site",
-      "Condition": {
-        "StringLike": {
-          "s3:prefix": ["lambda/*"]
-        }
-      }
-    },
-   {
-            "Effect": "Allow",
-            "Action": [
-              "s3:GetObject",
-              "s3:PutObject"
-            ],
-            "Resource": "arn:aws:s3:::stock-market-site/*"
-   },
-   {
-      "Effect": "Allow",
-      "Action": [
-        "iam:PassRole"
-      ],
-      "Resource": "${aws_iam_role.stock_market_lambda_role.arn}"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "lambda:CreateFunction",
-        "lambda:UpdateFunctionCode",
-        "lambda:UpdateFunctionConfiguration",
-        "lambda:GetFunction",
-        "lambda:DeleteFunction",
-        "lambda:InvokeFunction"
-      ],
-      "Resource": "arn:aws:lambda:*:*:function:predictions_lambda"
-    }
+   }
  ]
 }
 EOF
