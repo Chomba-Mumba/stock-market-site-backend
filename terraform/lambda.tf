cde018a947eb1d7d7db72a9b8f59d9793a01e9fc
Chomba-Mumba <chombaibex@gmail.com>
2025-02-23 14:18:26 +0000

    cicd: added hash to lambda and changed workflow dependencies.

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,8 +1,3 @@
-data "aws_s3_bucket_object" "predictions_lambda_zip" {
-  bucket = "stock-market-site"
-  key = "lambda/predictions_lambda.zip"
-}
-
 # predictions Lambda
 resource "aws_lambda_function" "predictions_lambda" {
   function_name = "predictions_lambda"
@@ -15,8 +10,9 @@ resource "aws_lambda_function" "predictions_lambda" {
   s3_key     = "lambda/predictions_lambda.zip"
   depends_on = [aws_iam_role.stock_market_lambda_role]
 
-  source_code_hash = data.aws_s3_bucket_object.predictions_lambda_zip.etag
+  source_code_hash = filesha256("lambda/predictions_lambda.zip")
 }
+
 resource "aws_cloudwatch_log_group" "predictions" {
   name = "/aws/lambda/${aws_lambda_function.predictions_lambda.function_name}"
 
