bbb876a50df5f8b5060baa4bc256dc8951e0158d
Chomba-Mumba <chombaibex@gmail.com>
2025-02-26 14:54:22 +0000

    infra: added lambda layer for deps

--- scripts/create_pkg.sh
+++ scripts/create_pkg.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 set -e
 
-mkdir -p lambda
+LAYER_PATH="lambda/layers/predictions_layer"
 
+mkdir -p "${LAYER_PATH}/python"
+
+#create layers zip
+pip install -r requirements.txt -t "${LAYER_PATH}/python"
+
+zip -r "${LAYER_PATH}.zip" "${LAYER_PATH}"
+
+echo "Zipped Lambda Layer ${LAYER_PATH}"
+
+#zip lambda functions
 for dir in *_lambda; do
     if [ -d "$dir" ]; then
         LAMBDA_NAME="$dir"
-        PACKAGE_DIR="lambda_dist_pkg"
         ZIP_FILE="lambda/${LAMBDA_NAME}.zip"
-
-        #remove old pkg
-        rm -rf ${PACKAGE_DIR}
-        mkdir -p ${PACKAGE_DIR}
-
-        #install deps
-        pip install -r "$dir/requirements.txt" -t ${PACKAGE_DIR}
-
-        #move the package directory
-        cp -r ${PACKAGE_DIR}/* "$dir" 
-
         #create zip
         zip -r "${ZIP_FILE}" "$dir"
 
         echo "created lambda package: ${ZIP_FILE}"
     fi
+done
+
+#upload lambda layers.
+echo "uploading lambda layers..."
+
+for zip_layer in lambda/layers*.zip do
+    echo "$zip_layer"
+    aws s3 cp "$zip_layer" "s3://stock-market-site/lambda/layers/$(basename "$zip_layer")"
 done
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -10,7 +10,18 @@ resource "aws_lambda_function" "predictions_lambda" {
   s3_key     = "lambda/predictions_lambda.zip"
   depends_on = [aws_iam_role.stock_market_lambda_role]
 
+  layers = [aws_lambda_layer_version.predictions_layer.arn, aws_lambda_layer_version.predictions_layer]
+
   source_code_hash = filesha256("lambda/predictions_lambda.zip")
+}
+
+resource "aws_lambda_layer_version" "predictions_layer" {
+  layer_name = "stock_market_predictions_layer"
+
+  s3_bucket  = "stock-market-site"
+  s3_key     = "lambda/layers/predictions_layer.zip"
+
+  compatible_runtimes = ["python3.9"]
 }
 
 resource "aws_cloudwatch_log_group" "predictions" {
