fe53602023b92f8a50c6cdd1f33365d5b1855440
Chomba-Mumba <chombaibex@gmail.com>
2025-02-26 09:14:00 +0000

    infra: changed lambda handler

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -2,7 +2,7 @@
 resource "aws_lambda_function" "predictions_lambda" {
   function_name = "predictions_lambda"
   role          = aws_iam_role.stock_market_lambda_role.arn
-  handler       = "predictions.lambda_handler"
+  handler       = "predictions_lambda.predictions.lambda_handler"
 
   runtime = "python3.9"
 
