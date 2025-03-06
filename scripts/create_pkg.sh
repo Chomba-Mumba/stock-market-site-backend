bf5ab9664cfafdddfd7af4a47a635bf4c811d74b
Chomba-Mumba <chombaibex@gmail.com>
2025-03-06 11:12:26 +0000

    cicd: updated script

--- scripts/create_pkg.sh
+++ scripts/create_pkg.sh
@@ -8,16 +8,13 @@ cd "predictions_lambda/"
 echo "building  docker image... in $(pwd)"
 ls -a
 
-#build image
-docker build -t predictions .
-
-#tag image
-docker tag "predictions" "${ECR_REPOSITORY}/predictions:latest"
+#build and tag image
+docker build -t "stock_market_prediction/lambda_functions/predictions:latest" .
 
 echo "pushing docker image to ecr: predictions"
 
 # Push the image to ECR
-docker push ${ECR_REPOSITORY}/predictions:latest
+docker push "stock_market_prediction/lambda_functions/predictions:latest"
 
 echo "finished pushing docker image"
 
